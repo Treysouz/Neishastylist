cdcc130e55bb63ac1bdae7d039ca972b0841006b
Treysouz <treysouz@gmail.com>
2025-10-13 19:34:34 -0400

    feat: [NEIS-2] - Readjust alignment for hero section

--- src/sections/hero/hero.tsx
+++ src/sections/hero/hero.tsx
@@ -1,10 +1,11 @@
 import SectionWrapper from "../section-wrapper";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
     <SectionWrapper
       id="hero"
-      className="bg-logo-dark h-dvh flex flex-col sm:flex-row">
+      className="bg-logo-dark h-dvh flex flex-col sm:flex-row space-x-16">
       {/* <div className="h-full w-full bg-[url(/hero.png)] bg-contain bg-no-repeat bg-top sm:bg-left">
         <div className="w-full h-full bg-black/25 flex flex-col items-center justify-center space-y-8">
           <p className="text-6xl text-white text-center">
@@ -18,14 +19,15 @@ export default function HeroSection() {
       </div> */}
 
       <div className="w-full bg-[url(/hero.png)] bg-cover bg-no-repeat bg-top"></div>
-      <div className="w-full h-full bg-logo-dark flex flex-col items-center justify-center space-y-12">
-        <p className="text-6xl text-white text-center leading-20">
+      <div className="w-full h-full bg-logo-dark flex flex-col  justify-center space-y-12 ">
+        <p className="text-6xl text-white  leading-20">
           If You Can Dream It, <br></br> We Can Achieve It
         </p>
-
-        <button className="btn btn-primary text-white btn-xl">
-          Book an Appointment
-        </button>
+        <div>
+          <button className="btn btn-primary text-white btn-xl">
+            Book an Appointment
+          </button>
+        </div>
       </div>
     </SectionWrapper>
   );

da43819e99f0d2036f6b63913dcb0130de13a96e
Treysouz <treysouz@gmail.com>
2025-10-16 15:51:54 -0400

    fix: [NEIS-4] - Added back base url for e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./e2e",
   timeout: 10000,
+  use: {
+    baseURL: "http://localhost:3000",
+  },
   webServer: {
     command: "npm run dev",
     url: "http://localhost:3000",

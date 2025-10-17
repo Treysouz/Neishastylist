70c9e77b6e53f65603508c0bceef3cb522be0eae
Treysouz <treysouz@gmail.com>
2025-10-17 14:07:30 -0400

    fix: [NEIS-7] - Used cloudflare's create tool

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,19 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-      "playwright-report/**",
-      "coverage/**",
-      ".open-next/**",
-      ".wrangler/**"
-    ],
-  },
 ];
 
 export default eslintConfig;

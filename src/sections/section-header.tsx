7dc3eb18eaf2477b32b046359011e26e711af0d4
Treysouz <treysouz@gmail.com>
2025-10-22 08:54:25 -0400

    fix: [NEIS-19] - Fixed linting errors

--- src/sections/section-header.tsx
+++ src/sections/section-header.tsx
@@ -5,7 +5,7 @@ interface SectionHeaderProps {
 
 export default function SectionHeader({ children }: SectionHeaderProps) {
   return (
-    <h2 className="text-2xl sm:text-4xl xl:text-5xl text-white tracking-widest">
+    <h2 className="text-2xl sm:text-4xl xl:text-5xl tracking-widest text-white font-medium">
       {children}
     </h2>
   );
--- src/sections/services/components/service-card.tsx
+++ src/sections/services/components/service-card.tsx
@@ -16,11 +16,11 @@ export default function ServiceCard({
 }: ServiceCardProps) {
   return (
     <Card className="bg-neutral p-4 lg:p-8 flex flex-col items-center justify-center space-y-4 lg:space-y-8 w-full sm:w-auto shadow-lg">
-      <Icon svg={svg} className="size-10 lg:size-16 text-accent"></Icon>
+      <Icon svg={svg} className="size-10 lg:size-16 text-accent" />
       <h3 className="text-lg sm:text-xl lg:text-2xl font-bold">{category}</h3>
       <ul className="text-sm lg:text-base list-disc pl-8">
         {services.map((service) => (
-          <li>{service}</li>
+          <li key={service}>{service}</li>
         ))}
       </ul>
     </Card>

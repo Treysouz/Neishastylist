2558faa3bfeb1010afff3569a16a53b8f25f2590
Treysouz <treysouz@gmail.com>
2025-10-22 09:44:48 -0400

    feat: [NEIS-21] - Added Staff section

--- src/sections/staff.tsx
+++
@@ -1,9 +0,0 @@
-import SectionWrapper from "./section-wrapper";
-
-export default function StaffSection() {
-  return (
-    <SectionWrapper id="staff" className="h-dvh">
-      staff
-    </SectionWrapper>
-  );
-}

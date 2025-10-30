dd7ecdfe3f11815556d1495ab25ec3a90e12e9e2
Treysouz <treysouz@gmail.com>
2025-10-30 13:55:40 -0400

    chore: [NEIS-14] - Fixed e2e test fail

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,8 +1,8 @@
-export { default as HeroSection } from "./hero/hero";
-export { default as WorkSection } from "./work";
-export { default as AboutSection } from "./about-us/about-section";
-export { default as ContactSection } from "./contact-us/contact-section";
-export { default as ServicesSection } from "./salon-services/salon-services";
-export { default as StaffSection } from "./staff";
+export { default as HeroSection } from "./hero-section/hero-section";
+export { default as WorkSection } from "./work-section/work-section";
+export { default as AboutSection } from "./about-section/about-section";
+export { default as ContactSection } from "./contact-section/contact-section";
+export { default as ServicesSection } from "./services-section/services-section";
+export { default as StaffSection } from "./staff-section/staff-section";
 export { default as SectionHeader } from "./section-header";
 export { default as SectionWrapper } from "./section-wrapper";
